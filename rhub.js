584f23ea85c56fc4145219aa246eacc7770bfcd6
mohcaster8 <miguel.castro1@decathlon.com>
2025-09-09 23:47:29 +0200

    Update rhub.js

--- rhub.js
+++ rhub.js
@@ -1,3 +1,6 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-app.js";
+import { getAuth, signInAnonymously, signInWithCustomToken, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-firestore.js";
 import { siteData } from './data.js';
 import { 
     collection, 
@@ -1949,3 +1952,4 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 
 });
+
